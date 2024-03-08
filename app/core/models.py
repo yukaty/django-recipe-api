9e226505fb0f165e118602f2b3dc0f2b0484cc2c
ytamura <ytamura@example.com>
2024-03-07 18:17:51 -0700

    Create User model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,49 @@
-from django.db import models # noqa
+"""
+Database models
+"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin
+)
+
+# UserManager is a class that comes with Django.
+# It provides helper functions for creating and managing users.
+class UserManager(BaseUserManager):
+    """Override the default UserManager to use email instead of username"""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError('User must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+# AbstractBaseUser provides the core implementation of a user model.
+# PermissionsMixin provides the core implementation of permissions.
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # Assign the UserManager to the objects attribute
+    # so that we can use the UserManager to create users.
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
